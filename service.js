71a34d3cd1af1d4b8f055948597c75a45440d041
nathannenga <nathan.nannenga@gmail.com>
2017-02-16 18:38:30 -0700

    donezo

--- service.js
+++ service.js
@@ -1,10 +1,24 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
+  // this.getUsers = function() {
+  //   return $http({
+  //       method: 'GET',
+  //       url: 'http://reqres.in/api/users?page=1'
+  //   })
+  // }
+
+  angular.module('userProfiles').service('mainService', function($http, $q) {
   this.getUsers = function() {
-    return $http({
+    var deferred = $q.defer();
+    $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
+    }).then(function(response) {
+      var parsedResponse = response.data.data
+      deferred.resolve(parsedResponse)
     })
+    return deferred.promise;
   }
+});
 
 });
